16250fe074b9b730134508750fef9d445dc5c37a
Mycaelai <walejandropg15@gmail.com>
2023-03-13 17:03:38 -0500

    Button note block

--- SFI/Assets/Scripts/BtnEvent.cs
+++ SFI/Assets/Scripts/BtnEvent.cs
@@ -5,8 +5,8 @@ using Uduino;
 public class BtnEvent : MonoBehaviour
 {
     public Text textzone1, textzone2, textzone3, textzone4;
-    public AudioSource source1, source2, source3, source4;
-    public float wait = 0.5f;
+    public AudioSource[] sources = new AudioSource[4];
+    public float[] crono = new[] {0.5f,0.5f,0.5f,0.5f };
     
     void Start(){
         UduinoManager.Instance.pinMode(14, PinMode.Input_pullup);
@@ -16,55 +16,35 @@ public class BtnEvent : MonoBehaviour
     }
     
     void Update(){
+        
         int bt1 = UduinoManager.Instance.digitalRead(14);
         int bt2 = UduinoManager.Instance.digitalRead(27);
         int bt3 = UduinoManager.Instance.digitalRead(26);
         int bt4 = UduinoManager.Instance.digitalRead(25);
+        
+        ButtonEvnt(bt1,textzone1,1);
+        ButtonEvnt(bt2,textzone2,2);
+        ButtonEvnt(bt3,textzone3,3);
+        ButtonEvnt(bt4,textzone4,4);
+    }
 
-        if (bt1 == 0 && wait > 0){
-            textzone1.text = "Down";
-            AudioTrigger(source1);
-            wait -= Time.deltaTime;
+    void ButtonEvnt(int btn, Text text, int i)
+    {
+        if (btn == 0 && crono[i-1] > 0){
+            text.text = "Down";
+            AudioTrigger(sources[i-1]);
+            crono[i-1] -= Time.deltaTime;
         }
         else{
-            textzone1.text = "Up";
-            if (bt1 == 1)
-            {
-                wait = 0.5f;
+            text.text = "Up";
+            if (btn == 1){
+                crono[i-1] = 0.5f;
             }
         }
-
-        
-        if (bt2 == 0){
-            textzone2.text = "Down";
-            AudioTrigger(source2);
-        }else
-            textzone2.text = "Up";
-        
-        if (bt3 == 0){
-            textzone3.text = "Down";
-            AudioTrigger(source3);
-        }else
-            textzone3.text = "Up";
-        
-        if (bt4 == 0){
-            textzone4.text = "Down";
-            AudioTrigger(source4);
-        }else
-            textzone4.text = "Up";
     }
-    void AudioTrigger(AudioSource aud)
-    {
+    void AudioTrigger(AudioSource aud){
         while (aud.isPlaying == false){
             aud.Play();
-        }
-    }
-
-    void ButtonTimer(float t)
-    {
-        if (t > 0)
-        {
-            t -= Time.deltaTime;
         }
     }
 }
