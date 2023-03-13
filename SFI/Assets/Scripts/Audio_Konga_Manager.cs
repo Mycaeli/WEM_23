6557179021f635317caadda26c43711923057320
Mycaelai <walejandropg15@gmail.com>
2023-03-13 12:46:18 -0500

    Add Esp32

---
+++ SFI/Assets/Scripts/Audio_Konga_Manager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio_Konga_Manager : MonoBehaviour
+{
+    public AudioSource Source;
+    public AudioClip clip;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ SFI/Assets/Scripts/BtnEvent.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.UI;
+using Uduino;
+
+public class BtnEvent : MonoBehaviour
+{
+    public Text textzone1, textzone2, textzone3, textzone4;
+    public AudioSource source1, source2, source3, source4;
+    
+    void Start(){
+        UduinoManager.Instance.pinMode(14, PinMode.Input_pullup);
+        UduinoManager.Instance.pinMode(27, PinMode.Input_pullup);
+        UduinoManager.Instance.pinMode(26, PinMode.Input_pullup);
+        UduinoManager.Instance.pinMode(25, PinMode.Input_pullup);
+    }
+    
+    void Update(){
+        int bt1 = UduinoManager.Instance.digitalRead(14);
+        int bt2 = UduinoManager.Instance.digitalRead(27);
+        int bt3 = UduinoManager.Instance.digitalRead(26);
+        int bt4 = UduinoManager.Instance.digitalRead(25);
+
+        if (bt1 == 0){
+            textzone1.text = "Down";
+            AudioTrigger(source1);
+        }else
+            textzone1.text = "Up";
+        
+        if (bt2 == 0){
+            textzone2.text = "Down";
+            AudioTrigger(source2);
+        }else
+            textzone2.text = "Up";
+        
+        if (bt3 == 0){
+            textzone3.text = "Down";
+            AudioTrigger(source3);
+        }else
+            textzone3.text = "Up";
+        
+        if (bt4 == 0){
+            textzone4.text = "Down";
+            AudioTrigger(source4);
+        }else
+            textzone4.text = "Up";
+    }
+    void AudioTrigger(AudioSource aud)
+    {
+        while (aud.isPlaying == false){
+            aud.Play();
+        }
+    }
+}
