8e2ed4018402af527961a7d8bd73797a69b8f3ab
mcaladp09 <theredstonefighter@gmail.com>
2023-05-09 21:03:16 -0500

    Revert "SFI 2 Upload"
    This reverts commit d8203ed4d0cd0523687a8f6b6f8cc24a186f9262.

---
+++ SFI/Assets/Assets/Instruments/Script/Script Music_Notes/script_FS_Note.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script_FS_Note : MonoBehaviour
+{
+    public AudioSource FS_Note;
+
+    private void OnMouseDown()
+    {
+        {
+            FS_Note.Play();
+            Console.Write("Note FS ON");
+        }
+    }
+}
---
+++ SFI/Assets/Assets/Instruments/Script/Script Music_Notes/script_GS_Note.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script_GS_Note : MonoBehaviour
+{
+    public AudioSource GS_Note;
+
+    private void OnMouseDown()
+    {
+        {
+            GS_Note.Play();
+            Console.Write("Note GS ON");
+        }
+    }
+}
